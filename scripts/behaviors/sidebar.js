b009d44bc8f86f24b23b9a703cafa9d496a7565f
Gino <gcanestrari@alumnos.unsada.edu.ar>
2022-10-25 20:41:19 -0300

    🩹 Arreglo de error en los botones del sidebar

--- scripts/behaviors/sidebar.js
+++ scripts/behaviors/sidebar.js
@@ -2,21 +2,28 @@
 const $items = document.querySelectorAll('.sidebar-item')
 const $contents = document.querySelectorAll('.content-box')
 
-
 $items.forEach((item, index) => {
   item.addEventListener('click', (event) => {
+    if (event.target.matches('.sidebar-item')) {
+      document.querySelector('.sidebar-item.-active').classList.remove('-active')
+      document.querySelector('.content-box.-active').classList.remove('-active')
+      event.target.classList.toggle('-active')
+      $contents[index].classList.toggle('-active')
+    }
+
     if (event.target.matches('.link')) {
       document.querySelector('.sidebar-item.-active').classList.remove('-active')
       document.querySelector('.content-box.-active').classList.remove('-active')
       event.target.parentNode.classList.toggle('-active')
+      $contents[index].classList.toggle('-active')
     }
     
     if (event.target.matches('.bx')) {
       document.querySelector('.sidebar-item.-active').classList.remove('-active')
       document.querySelector('.content-box.-active').classList.remove('-active')
       event.target.parentNode.parentNode.classList.toggle('-active')
+      $contents[index].classList.toggle('-active')
     }
 
-    $contents[index].classList.toggle('-active')
   })
 })

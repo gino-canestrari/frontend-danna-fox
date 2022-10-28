dd85036f7bd2961a7ca1cc1ee89a4873caf1113d
Gino <gcanestrari@alumnos.unsada.edu.ar>
2022-10-28 00:36:00 -0300

    🚚 Cambios en la estructura del proyecto

--- public/index.php
+++ public/index.php
@@ -6,13 +6,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   
   <!-- Scripts -->
-  <script defer src="./scripts/main.js" type="module"></script>
+  <script defer src="./public/scripts/main.js" type="module"></script>
   <!-- Favicon -->
   <link rel="shortcut icon" href="./resources/favicon.svg" type="image/x-icon">
   <!-- Reset Styles -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/reseter.css" />
   <!-- Own Styles -->
-  <link rel="stylesheet" href="styles/main.css">
+  <link rel="stylesheet" href="./public/styles/main.css">
   <!-- Boxicons -->
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
